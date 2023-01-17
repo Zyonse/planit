938e4cef863b3c644ec1679d18d3c9ecf6a219d4
planit <planit@tanjeffreyz.github.io>
2023-01-17 22:33:21 +0000

    Updated assignments

---
+++ data/assignments.js
@@ -0,0 +1,113 @@
+assignments = {
+  "MA 266 ": [
+    {
+      "title": "HW02 Handgrade",
+      "course": "MA 266 ",
+      "dueDate": "2023-01-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/484120",
+      "submitted": false
+    },
+    {
+      "title": "Handwritten HW3",
+      "course": "MA 266 ",
+      "dueDate": "2023-01-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/484120",
+      "submitted": false
+    },
+    {
+      "title": "Handwritten HW4",
+      "course": "MA 266 ",
+      "dueDate": "2023-01-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/484120",
+      "submitted": false
+    },
+    {
+      "title": "Handwritten HW5",
+      "course": "MA 266 ",
+      "dueDate": "2023-01-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/484120",
+      "submitted": false
+    },
+    {
+      "title": "Handwritten HW6",
+      "course": "MA 266 ",
+      "dueDate": "2023-01-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/484120",
+      "submitted": false
+    },
+    {
+      "title": "Handwritten HW8",
+      "course": "MA 266 ",
+      "dueDate": "2023-02-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/484120",
+      "submitted": false
+    },
+    {
+      "title": "Handwritten HW9",
+      "course": "MA 266 ",
+      "dueDate": "2023-02-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/484120",
+      "submitted": false
+    },
+    {
+      "title": "Handwritten HW10",
+      "course": "MA 266 ",
+      "dueDate": "2023-02-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/484120",
+      "submitted": false
+    },
+    {
+      "title": "Handwritten HW12",
+      "course": "MA 266 ",
+      "dueDate": "2023-02-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/484120",
+      "submitted": false
+    }
+  ],
+  "Spring 2023 ECE 20001 - Merge": [
+    {
+      "title": "HW 1 work submission",
+      "course": "Spring 2023 ECE 20001 - Merge",
+      "dueDate": "2023-01-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/482221/assignments/2536586/submissions/155804255",
+      "submitted": true
+    }
+  ],
+  "wl.202320.ENGR.13000.001": [
+    {
+      "title": "Design Challenge 1",
+      "course": "wl.202320.ENGR.13000.001",
+      "dueDate": "2023-01-11T13:20:00",
+      "link": "https://www.gradescope.com/courses/485151/assignments/2546272/submissions/155367211",
+      "submitted": true
+    },
+    {
+      "title": "ICA 1.2",
+      "course": "wl.202320.ENGR.13000.001",
+      "dueDate": "2023-01-11T13:30:00",
+      "link": "https://www.gradescope.com/courses/485151/assignments/2556838/submissions/155368380",
+      "submitted": true
+    },
+    {
+      "title": "HW 1 - EX 1 Individual",
+      "course": "wl.202320.ENGR.13000.001",
+      "dueDate": "2023-01-13T11:30:00",
+      "link": "https://www.gradescope.com/courses/485151/assignments/2556840/submissions/155595179",
+      "submitted": true
+    },
+    {
+      "title": "ICA 1.3",
+      "course": "wl.202320.ENGR.13000.001",
+      "dueDate": "2023-01-13T13:30:00",
+      "link": "https://www.gradescope.com/courses/485151/assignments/2568992/submissions/155609654",
+      "submitted": true
+    },
+    {
+      "title": "HW 2 - EX 2 Individual",
+      "course": "wl.202320.ENGR.13000.001",
+      "dueDate": "2023-01-20T11:30:00",
+      "link": "https://www.gradescope.com/courses/485151",
+      "submitted": false
+    }
+  ]
+};
