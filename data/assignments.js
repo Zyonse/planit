4df0a838ab178b3ba7c95854942ae3467bfc7294
planit <planit@tanjeffreyz.github.io>
2023-02-01 16:47:06 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -199,6 +199,13 @@ assignments = {
       "dueDate": "2023-02-06T11:30:00",
       "link": "https://www.gradescope.com/courses/485151",
       "submitted": false
+    },
+    {
+      "title": "ICA 4.2 ",
+      "course": "wl.202320.ENGR.13000.001",
+      "dueDate": "2023-02-01T13:30:00",
+      "link": "https://www.gradescope.com/courses/485151",
+      "submitted": false
     }
   ]
 };
