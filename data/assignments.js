c7cfb8507784a95caa1fc8e8b210ef92d66628dc
planit <planit@tanjeffreyz.github.io>
2023-03-07 19:38:09 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -167,8 +167,8 @@ assignments = {
       "title": "HW 8 work submission",
       "course": "Spring 2023 ECE 20001 - Merge",
       "dueDate": "2023-03-07T23:59:00",
-      "link": "https://www.gradescope.com/courses/482221",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/482221/assignments/2706908/submissions/167018037",
+      "submitted": true
     }
   ],
   "wl.202320.ENGR.13000.001": [
